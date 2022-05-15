92d41eb46e29007520fb43595ba1746a817a92bc
huzheng <openinx@gmail.com>
2022-05-15 10:49:34 +0800

    Add qsort impl

--- src/qsort.rs
+++ src/qsort.rs
@@ -1,16 +1,28 @@
 
-
-fn _sort_(v: &mut[i32], l: i32, r: i32) {
-    if l >= r {
-        return;
+fn _sort_(v: &mut[i32], l: usize, r: usize) {
+    let mut i = l;
+    let x = v[r];
+    for j in l..r {
+        if v[j] <= x {
+            v.swap(i, j);
+            i += 1;
+        }
     }
+    v.swap(i, r);
 
-    let mid = (l + r) >> 1;
+    if l < i {
+        _sort_(v, l, i-1);
+    }
+    if i < r {
+        _sort_(v, i+1, r);
+    }
 }
 
-fn qsort(v: &mut[i32]) {
-    // TODO: implement this method.
-    println!("{:?}", v);
+pub fn qsort(v: &mut[i32]) {
+    if v.len() <= 1{
+        return;
+    }
+    _sort_(v, 0, v.len() - 1);
 }
 
 #[cfg(test)]
@@ -20,8 +32,23 @@ mod tests{
     #[test]
     pub fn test_qsort(){
         let mut vec = vec![1, 2, 3];
-
         qsort(&mut vec);
         assert_eq!(vec, [1, 2, 3]);
+
+        vec = vec![3, 2, 1];
+        qsort(&mut vec);
+        assert_eq!(vec, [1, 2, 3]);
+
+        vec = vec![1, 1, 1];
+        qsort(&mut vec);
+        assert_eq!(vec, [1, 1, 1]);
+
+        vec = vec![2, 8, 4, 5, 7, 2, 5];
+        qsort(&mut vec);
+        assert_eq!(vec, [2, 2, 4, 5, 5, 7, 8]);
+
+        vec = vec![];
+        qsort(&mut vec);
+        assert_eq!(vec, []);
     }
 }
