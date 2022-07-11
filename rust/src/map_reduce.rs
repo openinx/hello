1cd13812cf4d7ec3cd54334dd340d2de030489d0
openinx <openinx@gmail.com>
2022-07-11 22:12:45 +0800

    Rust: Remove the FIXME in mapreduce.

--- rust/src/map_reduce.rs
+++ rust/src/map_reduce.rs
@@ -1,7 +1,9 @@
+use std::pin::Pin;
+
 struct MapReduce<T> {
     input: Vec<T>,
-    map: Option<Box<dyn Map>>,
-    reduce: Option<Box<dyn Reduce>>,
+    map: Option<Pin<Box<dyn Map>>>,
+    reduce: Option<Pin<Box<dyn Reduce>>>,
 }
 
 impl<T> MapReduce<T> {
@@ -13,15 +15,25 @@ impl<T> MapReduce<T> {
         }
     }
 
-    fn map<I, O, F>(&mut self, map: &dyn Map) {
+    fn map<F>(&mut self, map: F)
+    where
+        F: Map + 'static,
+    {
         // FIXME: Still don't get the correct approach to assign the
         // `map` to `self.map`  :-(
-        todo!()
+        self.map = Some(Box::pin(map));
     }
 
-    fn reduce<I, O, F>(&mut self, reduce: &dyn Reduce) {
+    fn reduce<F>(&mut self, reduce: F)
+    where
+        F: Reduce + 'static,
+    {
         // FIXME: Still don't get the correct approach to assign the
         // `reduce` to `self.reduce`  :-()
+        self.reduce = Some(Box::pin(reduce));
+    }
+
+    fn execute<O>(&mut self) -> O {
         todo!()
     }
 }
